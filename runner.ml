f6720c8b17b7c34161ef37b55e7cc7c17b6de239
metalgearay <nah@nah.com>
2017-07-31 14:47:06 -0400

    Can now actually open files instead of having to pipe

--- runner.ml
+++ runner.ml
@@ -1,6 +1,7 @@
-
-
+let file = Sys.argv.(1)
+let ic = open_in file 
 let _ =
-  let lexbuf = Lexing.from_channel stdin in
+  let lexbuf = Lexing.from_channel ic in
   let ast = Parser.program Lexer.token lexbuf in 
-	print_string (Ast.string_of_program ast)
+	print_string (Ast.string_of_program ast);
+	close_in ic
