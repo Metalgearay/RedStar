ddca94941efb2c0686100e571196df27dae567b6
metalgearay <nah@nah.com>
2017-08-01 00:20:41 -0400

    Small tweek so that the module is dumped to an .ll file

--- runner.ml
+++ runner.ml
@@ -1,9 +1,10 @@
 let file = Sys.argv.(1)
+let output_file = file^".ll"
 let ic = open_in file 
 let _ =
   let lexbuf = Lexing.from_channel ic in
   let ast = Parser.program Lexer.token lexbuf in 
 	let m = Codegen.ir_dump ast in
 	Llvm_analysis.assert_valid_module m;
-	print_string(Llvm.string_of_llmodule m);
+	Llvm.print_module output_file m;
 	close_in ic
